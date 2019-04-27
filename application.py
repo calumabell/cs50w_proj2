0750d9d8bab8bb88c40d145527e853274f908f5c
Calum Bell <calumabell@googlemail.com>
2019-04-27 14:05:26 +0100

    added handlebars templating to generate HTML for messages

--- application.py
+++ application.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-
+# Declare server-side memory
 chatLog = []
 logSize = 10
 
 @app.route("/")
 def index():
-    return render_template("index.html", messages=chatLog)
+    return render_template("index.html")
 
 @socketio.on('submit message')
 def msg(data):
@@ -22,8 +22,15 @@ def msg(data):
     chatLog.insert(0, data)
     if len(chatLog) >= logSize:
         chatLog.pop(logSize)
-
     emit('new message', data, broadcast=True)
+
+@socketio.on('get chatLog')
+def getHistory():
+
+    # Iterate through history backwards so that newest messages are top
+    for i in range(len(chatLog), 0, -1):
+        emit('new message', chatLog[i-1])
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,17 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Conntect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port)
+    const socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port)
+
+    // Template for message card
+    const template = Handlebars.compile(document.querySelector('#cardTemplate').innerHTML)
+
+    let name = localStorage.getItem('name')
+    if (!name) {
+        name = prompt("What is your name?")
+        localStorage.setItem('name', name)
+    }
 
     // When connected, configure listeners
     socket.on('connect', () => {
 
+        socket.emit('get chatLog')
         // Entering a message into the form emits a 'submit message' event
         document.querySelector('#submitMessage').onsubmit = () => {
 
             const message = document.querySelector('#newMessage').value
             const d = new Date()
             document.querySelector('#newMessage').value = ""
-            socket.emit('submit message', {'msg': message, 'timestamp': d.toUTCString()})
+            socket.emit('submit message', {'name': name, 'msg': message, 'timestamp': d.toUTCString()})
 
             // return false to stop page from reloading
             return false
@@ -19,11 +29,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // When a new message is received, append it to the DOM
         socket.on('new message', data => {
-            const li = document.createElement('li')
-            li.innerHTML = data.msg + ' ' + data.timestamp
-            const list = document.getElementById("messages")
-            list.insertBefore(li, list.childNodes[0])
+            // Use message info from data param and render template
+            const context = {"name": data.name, "timestamp": data.timestamp, "message": data.msg}
+            const card = template(context)
+            // Add message to DOM (add new messages to top of list)
+            document.getElementById("messages").innerHTML = card + document.getElementById("messages").innerHTML
         })
     })
-
 })
